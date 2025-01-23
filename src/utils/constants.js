b6161682b944a2d428ce7b26023cc76d033fc450
abhinavkayarkar <abhinavkayarkar@gmail.com>
2025-01-23 10:11:32 +0530

    search url updated

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,6 +7,6 @@ export const USER_ICON = "https://cdn-icons-png.flaticon.com/128/64/64572.png";
 export const YOUTUBE_API_URL = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${process.env.REACT_APP_GOOGLE_API_KEY}`;
 
 export const YOUTUBE_SEARCH_URL =
-  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+  "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
 export const COUNT_VALUE = 250;
